09ec455dd508280e236dcbe5ce8cba49021b2572
John Reed <87283488+JohnReedV@users.noreply.github.com>
2023-04-18 12:38:15 -0700

    rust!

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ client.on('message', async (msg) => {
         const response = await chatGPT(prompt)
         msg.channel.send(`<@${msg.author.id}> ${response}`)
     } else if (msg.author.username == "Crawdog") {
-        const prompt = `You can only speak in code. Reply to this prompt: "${msg.content}"`
+        const prompt = `You can only speak in rust code. Reply to this prompt: "${msg.content}"`
         const response = await chatGPT(prompt)
         msg.channel.send(`<@${msg.author.id}> ${response}`)
     } else if (msg.author.username == "Paradox Rift") {
