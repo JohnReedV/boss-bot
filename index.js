26f958e7bfd07ec8c2b1720eb107436f1a950654
JohnReedV <jjreedv@protonmail.com>
2023-09-05 21:11:02 -0700

    more bug fix

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ async function chatGPT(prompt) {
 
     try {
         const response = await openai.chat.completions.create(params)
-        console.log(response.choices[0].message)
         return response.choices[0].message
     } catch (error) {
         console.error('Error interacting with ChatGPT:', error)
@@ -34,39 +33,39 @@ client.on('messageCreate', async (msg) => {
     console.log(`Received message: ${msg.content}`)
 
     if (msg.content.startsWith('ye') || msg.content.startsWith('Ye') && !msg.author.bot) {
-        const prompt = msg.content.slice(5).trim()
+        const prompt = msg.content.slice(4).trim()
         const response = await chatGPT(prompt)
         msg.channel.send(response)
     } else if (msg.author.username == "pryceless3") {
         const prompt = `You are a professional insulter.
         Everything you say is known to be ironic. Insult this "${msg.content}"`
         const response = await chatGPT(prompt)
-        msg.channel.send(`<@${msg.author.id}> ${response.content}`)
+        msg.channel.send(`<@${msg.author.id}> ${response.choices[0].message.content.substring(0, 1998)}`)
     } else if (msg.author.username == "Dolphin") {
         const prompt = `Reply as if you are madly in love with the prompt sender.
         The prompt: "${msg.content}"`
         const response = await chatGPT(prompt)
-        msg.channel.send(`<@${msg.author.id}> ${response.content}`)
+        msg.channel.send(`<@${msg.author.id}> ${response.choices[0].message.content.substring(0, 1998)}`)
     } else if (msg.author.username == "Paradox Rift") {
         const prompt = `You are a professional insulter.
         Everything you say is known to be ironic. Insult this "${msg.content}"`
         const response = await chatGPT(prompt)
-        msg.channel.send(`<@${msg.author.id}> ${response.content}`)
+        msg.channel.send(`<@${msg.author.id}> ${response.choices[0].message.content.substring(0, 1998)}`)
     } else if (msg.author.username == "Crawdog") {
         const prompt = `Reply to this prompt as if you are a crazed conspiracy theorist.
         Prompt: "${msg.content}"`
         const response = await chatGPT(prompt)
-        msg.channel.send(`<@${msg.author.id}> ${response.content}`)
+        msg.channel.send(`<@${msg.author.id}> ${response.choices[0].message.content.substring(0, 1998)}`)
     } else if (msg.author.username == "efeld") {
         const prompt = `Reply to this prompt as if you are eric cartman. Prompt: "${msg.content}"`
         const response = await chatGPT(prompt)
-        msg.channel.send(`<@${msg.author.id}> ${response.content}`)
+        msg.channel.send(`<@${msg.author.id}> ${response.choices[0].message.content.substring(0, 1998)}`)
     } else if (msg.author.username == "ozinator11") {
         msg.channel.send(`<@${msg.author.id}> gay`)
     } else if (msg.author.username == "drlangeschlange" || msg.author.username == "TheDankGuy") {
         const prompt = `Reply to this prompt as if you are having a discussion with a very important man: "${msg.content}"`
         const response = await chatGPT(prompt)
-        msg.channel.send(`<@${msg.author.id}> ${response.content}`)
+        msg.channel.send(`<@${msg.author.id}> ${response.choices[0].message.content.substring(0, 1998)}`)
     }
 })
 
