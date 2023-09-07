bfa35827f61d5e43182541425674f8e7e2f57296
JohnReedV <jjreedv@protonmail.com>
2023-09-06 19:46:34 -0700

    add audio queue

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use lazy_static::lazy_static;
 use openai_rust::{chat::ChatArguments, chat::Message as OpenAiMessage, Client as OpenAiClient};
 use regex::Regex;
 use serenity::{
@@ -8,8 +9,12 @@ use serenity::{
     prelude::*,
 };
 use songbird::{SerenityInit, Songbird};
-use std::{env, sync::Arc};
-use tokio::process::Command as TokioCommand;
+use std::{collections::VecDeque, env, process::Command, sync::Arc, time::Duration};
+use tokio::{process::Command as TokioCommand, sync::Mutex, time::sleep};
+
+lazy_static! {
+    static ref VIDEO_QUEUE: Mutex<VecDeque<String>> = Mutex::new(VecDeque::new());
+}
 
 pub struct SongbirdKey;
 
@@ -25,7 +30,7 @@ async fn main() {
     let framework = StandardFramework::new().configure(|c| c.prefix("~"));
 
     let mut client = Client::builder(&token, GatewayIntents::all())
-        .event_handler(Handler)
+        .event_handler(Handler::default())
         .framework(framework)
         .register_songbird()
         .await
@@ -43,51 +48,87 @@ async fn main() {
         .map_err(|why| println!("Client ended: {:?}", why));
 }
 
-struct Handler;
+struct Handler {
+    pub playing: Arc<Mutex<bool>>,
+}
+
+impl Default for Handler {
+    fn default() -> Self {
+        Handler {
+            playing: Arc::new(Mutex::new(false)),
+        }
+    }
+}
+
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        let mut message: &str = msg.content.trim();
+        let mut playing: tokio::sync::MutexGuard<'_, bool> = self.playing.lock().await;
 
+        let mut message: &str = msg.content.trim();
         if msg.author.bot {
             return;
-        } else if message.starts_with("ye https://") || message.starts_with("Ye https://") {
+        }
+
+        if message.starts_with("ye https://") || message.starts_with("Ye https://") {
             message = message.split_at(3).1;
 
-            let guild_id = msg.guild_id.unwrap();
-            let guild = ctx.cache.guild(guild_id).unwrap();
-            let channel_id = guild
-                .voice_states
-                .get(&msg.author.id)
-                .and_then(|voice_state| voice_state.channel_id);
-            match channel_id {
-                Some(channel) => {
-                    let manager = songbird::get(&ctx)
-                        .await
-                        .expect("Songbird Voice client placed in at initialization.")
-                        .clone();
+            println!("Got yt message: {}", message);
+            match extract_youtube_url(message) {
+                Ok(url) => {
+                    {
+                        let mut queue = VIDEO_QUEUE.lock().await;
+                        queue.push_back(url.clone());
+                    }
+                    let guild_id = msg.guild_id.unwrap();
+                    let guild = ctx.cache.guild(guild_id).unwrap();
+                    let channel_id = guild
+                        .voice_states
+                        .get(&msg.author.id)
+                        .and_then(|voice_state| voice_state.channel_id);
 
-                    let (_handler_lock, success) = manager.join(guild_id, channel).await;
+                    match channel_id {
+                        Some(channel) => {
+                            let manager = songbird::get(&ctx)
+                                .await
+                                .expect("Songbird Voice client placed in at initialization.")
+                                .clone();
 
-                    if success.is_ok() {
-                        match extract_youtube_url(message) {
-                            Ok(url) => {
-                                let _ = play_youtube(&ctx, msg, url)
-                                    .await
-                                    .expect("Expected to play_youtube");
+                            let (_handler_lock, success) = manager.join(guild_id, channel).await;
+                            if success.is_ok() {
+                                loop {
+                                    let should_continue: bool;
+                                    {
+                                        let queue = VIDEO_QUEUE.lock().await;
+                                        should_continue = queue.front().is_some();
+                                    }
+
+                                    if should_continue {
+                                        if !*playing { //DOES WORK??
+                                            *playing = true;
+                                            let _ = play_youtube(&ctx, msg.clone()).await;
+                                            let _ = sleep_for_video_duration(&url).await;
+                                            *playing = false;
+                                        }
+                                    } else {
+                                        break;
+                                    }
+                                }
                             }
-                            Err(_) => {
-                                let _ = msg.reply(&ctx, "Bad URL").await;
-                            }
+                        }
+                        None => {
+                            let _ = msg.reply(&ctx, "Join voice noob").await;
                         }
                     }
                 }
-                None => {
-                    let _ = msg.reply(&ctx, "Join a voice channel noob").await;
+                Err(_) => {
+                    let _ = msg.reply(&ctx, "Bad URL").await;
                 }
             }
         } else if message.starts_with("Ye") || message.starts_with("ye") {
             message = message.split_at(3).1;
+            println!("Got message: {}", message);
+
             let api_key: String = env::var("OPENAI_KEY").expect("Expected OPENAI_KEY to be set");
             let prompt: String = message.to_string();
 
@@ -108,38 +149,41 @@ impl EventHandler for Handler {
 async fn play_youtube(
     ctx: &Context,
     msg: Message,
-    url: String,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let guild = msg.guild(&ctx.cache).unwrap();
     let guild_id = guild.id;
 
     let manager = songbird::get(ctx)
         .await
-        .expect("Songbird Voice client placed in at initialisation.")
+        .expect("Songbird Voice client placed in at initialization.")
         .clone();
 
     if let Some(handler_lock) = manager.get(guild_id) {
         let mut handler = handler_lock.lock().await;
 
-        let output = TokioCommand::new("yt-dlp")
-            .arg("-f")
-            .arg("bestaudio")
-            .arg("-g")
-            .arg(&url)
-            .output()
-            .await?;
-        let audio_url = String::from_utf8(output.stdout)?.trim().to_string();
+        let mut queue = VIDEO_QUEUE.lock().await;
+        if let Some(url) = queue.pop_front() {
+            let output = TokioCommand::new("yt-dlp")
+                .arg("-f")
+                .arg("bestaudio")
+                .arg("-g")
+                .arg(&url)
+                .output()
+                .await?;
 
-        let source = match songbird::ffmpeg(audio_url).await {
-            Ok(source) => source,
-            Err(why) => {
-                println!("Err starting source: {:?}", why);
-                let _ = msg.channel_id.say(&ctx.http, "Can't play that one").await;
-                return Ok(());
-            }
-        };
+            let audio_url = String::from_utf8(output.stdout)?.trim().to_string();
 
-        handler.play_source(source);
+            let source = match songbird::ffmpeg(audio_url).await {
+                Ok(source) => source,
+                Err(why) => {
+                    println!("Err starting source: {:?}", why);
+                    let _ = msg.channel_id.say(&ctx.http, "Can't play that one").await;
+                    return Ok(());
+                }
+            };
+
+            handler.play_source(source);
+        }
     }
 
     Ok(())
@@ -202,4 +246,48 @@ fn extract_youtube_url(input: &str) -> Result<String, Box<dyn std::error::Error 
 fn is_valid_youtube_url(url: &str) -> bool {
     let re = Regex::new(r"https?://(www\.)?youtube\.com/watch\?v=[a-zA-Z0-9_-]+").unwrap();
     return re.is_match(url);
+}
+
+async fn sleep_for_video_duration(video_url: &str) -> std::io::Result<()> {
+    let output = Command::new("yt-dlp")
+        .arg("--get-duration")
+        .arg(video_url)
+        .output()?;
+
+    if output.status.success() {
+        let duration_str = String::from_utf8(output.stdout).unwrap();
+        let duration_parts: Vec<&str> = duration_str.trim().split(":").collect();
+        let duration = match duration_parts.len() {
+            3 => {
+                let hrs: u64 = duration_parts[0].parse().unwrap();
+                let mins: u64 = duration_parts[1].parse().unwrap();
+                let secs: u64 = duration_parts[2].parse().unwrap();
+                Duration::from_secs(hrs * 3600 + mins * 60 + secs)
+            }
+            2 => {
+                let mins: u64 = duration_parts[0].parse().unwrap();
+                let secs: u64 = duration_parts[1].parse().unwrap();
+                Duration::from_secs(mins * 60 + secs)
+            }
+            1 => {
+                let secs: u64 = duration_parts[0].parse().unwrap();
+                Duration::from_secs(secs)
+            }
+            _ => {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Couldn't parse duration",
+                ))
+            }
+        };
+
+        sleep(duration).await;
+    } else {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "yt-dlp failed to get video duration",
+        ));
+    }
+
+    Ok(())
 }
