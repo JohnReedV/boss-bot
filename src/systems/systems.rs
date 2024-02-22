6c7155c41abc23e1ce3dd683181e47bba1d9822a
John Reed <87283488+JohnReedV@users.noreply.github.com>
2024-02-22 11:16:42 -0800

    rm dalle_image

--- src/systems/systems.rs
+++ src/systems/systems.rs
@@ -8,10 +8,7 @@ use reqwest::Client;
 use serde_json::json;
 use serde_json::Value;
 use serenity::{
-    model::{
-        channel::Message,
-        prelude::{GuildId, ReactionType},
-    },
+    model::{channel::Message, prelude::GuildId},
     prelude::Context,
 };
 use songbird::Songbird;
@@ -20,10 +17,7 @@ use std::{
     collections::VecDeque,
     sync::{atomic::Ordering, Arc},
 };
-use tokio::{
-    fs,
-    time::{sleep, Duration},
-};
+use tokio::fs;
 
 pub async fn skip_all_enabled(app: &Handler, guild_id: GuildId, manager: Arc<Songbird>) {
     if let Some(handler_lock) = manager.get(guild_id) {
@@ -119,91 +113,6 @@ pub async fn get_image_path(name: String) -> Result<String, Error> {
         std::io::ErrorKind::NotFound,
         format!("Error with prefix {} not found", name),
     ))
-}
-
-pub async fn dalle_image(ctx: Context, msg: Message, api_key: &str, prompt: &str) -> String {
-    let client = Client::new();
-    let url = "https://api.openai.com/v1/images/generations";
-
-    let bot_msg = msg
-        .reply(&ctx, "Select a size")
-        .await
-        .expect("Expected prompt message");
-
-    let sizes = vec!["🟦", "↔", "↕️"];
-    for emoji in &sizes {
-        bot_msg
-            .react(&ctx.http, ReactionType::Unicode(emoji.to_string()))
-            .await
-            .expect("Expected to react");
-    }
-
-    let mut selected_size = None;
-    while selected_size.is_none() {
-        for emoji in &sizes {
-            let reaction_type = ReactionType::Unicode(emoji.to_string());
-            if let Ok(users) = bot_msg
-                .reaction_users(&ctx.http, reaction_type, None, None)
-                .await
-            {
-                if users.iter().any(|user| user.id == msg.author.id) {
-                    selected_size = match *emoji {
-                        "🟦" => Some("1024x1024"),
-                        "↔" => Some("1792x1024"),
-                        "↕️" => Some("1024x1792"),
-                        _ => None,
-                    };
-                    break;
-                }
-            }
-        }
-        if selected_size.is_none() {
-            sleep(Duration::from_secs(1)).await;
-        }
-    }
-
-    bot_msg.delete(&ctx).await.expect("expected to delete");
-    let size = selected_size.unwrap().to_string();
-
-    let bot_msg_2 = msg.reply(&ctx, "Generating...").await.unwrap();
-
-    match client
-        .post(url)
-        .bearer_auth(api_key)
-        .json(&serde_json::json!({
-            "prompt": prompt,
-            "model": "dall-e-3",
-            "n": 1,
-            "quality": "hd",
-            "size": size
-
-        }))
-        .send()
-        .await
-    {
-        Ok(response) => match response.text().await {
-            Ok(response_body) => {
-                bot_msg_2.delete(&ctx).await.unwrap();
-                let json: Value = match serde_json::from_str(&response_body) {
-                    Ok(json) => json,
-                    Err(_) => return "Failed to parse JSON".to_string(),
-                };
-
-                return json["data"][0]["url"]
-                    .as_str()
-                    .unwrap_or(json["error"]["message"].to_string().as_str())
-                    .to_string();
-            }
-            Err(_) => {
-                bot_msg_2.delete(&ctx).await.unwrap();
-                return "Failed to get response text from JSON".to_string();
-            }
-        },
-        Err(_) => {
-            bot_msg_2.delete(&ctx).await.unwrap();
-            return "Failed to send request".to_string();
-        }
-    }
 }
 
 pub async fn say_queue(msg: Message, ctx: &Context, queue: VecDeque<Node>) {
